29d67a8c7fe086228c70d2dc6ec355c4b7939ec3
“rohithgopala44” <rohithgopala44@gmail.com>
2023-06-02 09:07:44 +0530

    Added: Programs Of Logical Programs

---
+++ Perfect_Number/Perfect_Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogicalProgram
+{
+    class PerfectNumber
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, n;
+            Console.Write("Enter a Number :");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Prime_Number/Prime_Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LogicalPrograms {
+    public class PrimeNumber {
+        public static void Main(String[] args)
+        {
+
+
+            int n = 5, a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
